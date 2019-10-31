9a2df9415442ff5b01108aa95cb3751f8944fbec
schaefmk <schaefmk@rose-hulman.edu>
2019-10-31 04:51:15 -0400

    settings page up to date

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -29,5 +29,27 @@ rh.ClassName = class {
 /* Main */
 $(document).ready(() => {
 	console.log("Ready");
+	if($("#login-page").length){
+		console.log("On log in page");
+		new rh.LogInPageController();
+	} else if($("#chaunceys-page").length){
+		console.log("On Chauncey's page");
+		new rh.ChaunceysPageController();
+	} else if($("#union-page").length){
+		console.log("On Union Cafe page");
+		new rh.UnionPageController();
+	} else if($("#rg-page").length){
+		console.log("On Rose Gardens page");
+		new rh.RGPageController();
+	} else if($("#mc-page").length){
+		console.log("On Moench Cafe page");
+		new rh.MCPageController();
+	} else if($("#beanies-page").length){
+		console.log("On Beanies page");
+		new rh.BeaniesPageController();
+	} else if($("#settings-page").length){
+		console.log("On Settings page");
+		new rh.SettingsPageController();
+	}
 	
 });
